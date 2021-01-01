b087e0d5516cb70eae5f270a970ce34b60ddefe1
andeha <andeha@users.noreply.github.com>
2021-01-01 22:14:52 +0100

    Create 🥽⋆Newarks₂.cpp

---
+++ 🥽⋆Newarks₂.cpp
@@ -0,0 +1,12 @@
+/*  Newarks₂.cpp ║ /USB-SE/ and with a laptop accessory. */
+
+import PlatesAndGrill; /* with vt100 terminal. */
+
+/* xcrun clang @ccargs_mac -o Newarks₂ '🥽⋆Newarks₂.cpp' */
+
+int EstablishPeermeshAndControl() { return 0; }
+int EstablishVideolink() { return 0; }
+int IllustrateBattery() { return 0; }
+
+int main() { return 0; }
+
