fa8d922a8eafccd25d0d338a08ebb7c5b4881588
andeha <andeha@users.noreply.github.com>
2021-07-21 13:48:01 +0200

    Improved exposition.

---
+++ 􀖆 2-knapp.cpp
@@ -0,0 +1,14 @@
+/*  􀪥 2-knapp.cpp | taktil och modulativ. */
+
+import Terminalfun;
+
+int Measure₋Henry(int x, int y, void (^inductance)(double sample)) { return 0; }
+
+int main(
+  int argc, 
+  const char * argv[]
+)
+{
+  return 0;
+}
+
