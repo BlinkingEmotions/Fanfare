7fb3e9bc071756e8b79df862b51dfbc08acc8b34
andeha <andeha@users.noreply.github.com>
2021-12-27 00:48:04 +0100

    Improved exposition.

---
+++ 􀐒 retrospect.c
@@ -0,0 +1,73 @@
+/*  􀆖􀓤 retrospect.c | a language after swift. */
+
+import Twinbeam;
+
+/*
+  
+  import Twinbeam
+  
+  .partial /‌* 'struct' *‌/ after9₋variables
+  .end
+  
+  .definite /​* big₋endian₋struct *​/ 4kbframe
+  .end
+  
+  .union Tetra𝘖rUnicode
+    int32_t count, char32_t uc
+  .end
+  
+  á₋priori To₋precision(half x) -> definite sequent
+  á₋priori 𝟽₋op₋bytereverse(uint8_t b) -> uint8_t
+  
+  typedef __builtin_int_t Nonabsolute, structa₋middle₋index
+  
+  .compute booth₋multiply, [multiple] multiply
+  .signature (definite sequent x₁, definite sequent x₂) -> definite sequent
+  .additions ACC, mask, X1, X2 as 128 bit signed, hi,lo=0 as int, 
+    y={ .detail.bits=0, .valid=0 } as definite sequent
+  START(multiply)
+multiply:
+again:
+  
+  compare shift == 127 { }
+  branch again
+  
+  END(multiply)
+  
+  .compute int₋to₋sequent
+  .signature (int64_t ℤ, indirect definite sequent ℝ) -> ΨΛΩ
+  .additions neg as int16_t
+  START(int₋to₋sequent)
+int₋to₋sequent:
+  compare ℤ < 0 { ℤ = -ℤ; LI16 neg, 0; SEH neg }
+  END(int₋to₋sequent)
+  
+  .compute normalize_sequent
+  .signature (voluntary definite sequent x₁, voluntary definite sequent x₂) -> ΨΛΩ
+  START(normalize_sequent)
+normalize_sequent:
+  uint64_t top = Modulo(x₂->detail.bits>>64,uint64_t)
+  uint64_t right₋shift₋steps = 64 - __builtin_clzll(top)
+  /‌* { right₋shift₋steps += 1 } when Modulo(x₂->detail.bits,uint64_t) == 0x80000000 *‌/
+  END(normailze_sequent)
+  
+  .compute print₋sequent
+  .signature (int neg, direct definite 𝟽bit₋text integers, direct definite 
+    7bit₋text fracts, void (^zero₋alt₋₋nonused)(), void (^nonvalid)()) -> ΨΛΩ
+  START(print₋sequent)
+print₋sequent:
+  compare 𝕏
+  END(print₋sequent)
+ 
+ */
+
+int
+main(
+  int argc, 
+  const char * argv[]
+)
+{
+   return 0;
+}
+
+
