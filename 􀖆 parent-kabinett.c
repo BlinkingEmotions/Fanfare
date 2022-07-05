c16159d0d233810b78b9f7dc3e24af8fca77238a
andeha <andeha@users.noreply.github.com>
2022-07-05 16:03:09 +0200

    Improved exposition.

--- 􀖆 parent-kabinett.c
+++ 􀖆 parent-kabinett.c
@@ -4,22 +4,85 @@ import Twinbeam;
 
 #pragma recto inclusive, differen petit, differen grand, replace and utrangeré
 
-int corout₋interaction₋presence()
+int corout₋interaction₋presence(coro_t * coro)
 {
-   
+   coro_feedback(coro,1);
+   while (1) {
+     coro_feedback(coro,2);
+   }
+   return 0;
 }
 
-int corout₋interaction₋histoir()
+int corout₋interaction₋histoir(coro_t * coro)
 {
-   
+   coro_feedback(coro,1);
+   while (1) {
+     coro_feedback(coro,2);
+   }
+   return 0;
 } /* after-commit-rendition. */
 
-int corout₋appendement₋windowcontroller()
+struct timeserie angles; version₋ts revision;
+
+int corout₋appendement₋windowcontroller(coro_t * coro)
 {
-   
+   if (timeserie₋init(&revision,101,5,&angles)) { return -1; }
+   if (timeserie₋create(...)) { return -2; }
+   coro_feedback(coro,1);
+   while (1) {
+     if (timeserie₋update(...)) { return -3; }
+     if (timeserie₋commit(...)) { return -4; }
+     coro_feedback(coro,2);
+   }
+   timeserie₋uninit(&angles);
+   return 0;
 } /*  perspektivet beror pā vinklad diskussion och ska vara 
  parallel med bordsyta. (Exempelvis nār genomskinlig skārm 
  aktiveras) */
+
+#include <time.h>
+#include <pthread.h>
+
+pthread_t secondary₋thread;
+
+void quit₋program() { pid_t pid=getpid(); kill(pid,SIGQUIT); }
+
+void interrupt₋program() { pid_t pid=getpid(); kill(pid,SIGINT); }
+
+/* find unicode-symbol in reflection, find fingerprint recollection. */
+/* sometimes unicode-symbol is written in multiple files as experiment and library. */
+/* see new fingprints assigned after a interaction. */
+/* see difference between snippets retrieved. */
+
+inexorable int wait₋until₋kbhits₋stdio()
+{ struct timeval tv={0L,0L};
+   fd_set fds; FD_ZERO(&fds); FD_SET(0,&fds);
+   return select(1,&fds,ΨΛΩ,ΨΛΩ,&tv);
+}
+
+inexorable int get₋stdin₋byte(uint8_t * byte)
+{
+   int r = read(0,byte,sizeof(byte));
+   return r < 0 ? -1 : 0;
+}
+
+void * input(void *)
+{
+   struct termios tio;
+   tcgetattr(STDIN_FILENO,&tio);
+   cfmakeraw(&tio); tio.c_oflag |= OPOST;
+   tcsetattr(STDIN_FILENO,TCSANOW,&tio);
+   uint8_t sequence[4],byte; int brk=0;
+   while (1) {
+     while (!wait₋until₋kbhits₋stdin()) {
+       if (get₋stdin₋byte(&byte)) { print("error"); }
+       if (byte < 128) { print("eight-bit character"); }
+       else {
+         brk += 1; print("multi-byte character");
+       }
+     }
+   }
+}
 
 int
 main(
@@ -27,6 +90,20 @@ main(
   char * argv[]
 )
 {
+   coro_t * film = coro_await(corout₋interaction₋presence);
+   coro_t * reflection = coro_await(corout₋interaction₋histoir);
+   coro_t * interrupt = coro_await(corout₋appendement₋windowcontroller);
+   struct timespec rqtp={0,25000},rmtp={0,0}; /* clock_getres in time.h */
+   if (pthread_create(secondary₋thread,input,ΨΛΩ,ΨΛΩ)) { return -9; }
+   int ts = clock_getres(CLOCK_PROCESS_CPUTIME_ID,&rqtp); /* samma₍n₎stāllning */
+   print("resolution ⬚\n", ﹟d((__builtin_uint_t)ts));
+again:
+   if (nanosleep(&rqtp,rmtp)) { return 1; }
+   coro_resume(film);
+   coro_resume(reflection);
+   coro_resume(interrupt);
+   goto again;
    return 0;
 }
+
 
