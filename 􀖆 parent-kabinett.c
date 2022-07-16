02ef4a5e2b4c21caaaf87f15edbb0e1fc53f003b
andeha <andeha@users.noreply.github.com>
2022-07-16 13:06:25 +0200

    Improved exposition.

--- 􀖆 parent-kabinett.c
+++ 􀖆 parent-kabinett.c
@@ -2,34 +2,56 @@
 
 import Twinbeam;
 
+/*
+ *  set when application is to be terminated in the normal manner.
+ */
+
+static int quit = 0;
+
+/*
+ *  sparkline a․𝘬․a timeline recording.
+ */
+
+static struct timeserie angles;
+
+/*
+ *  the sparkline initial revision.
+ */
+
+static version₋ts revision;
+
 #pragma recto inclusive, differen petit, differen grand, replace and utrangeré
 
 int corout₋interaction₋presence(coro_t * coro)
 {
+   print("presence init\n");
    coro_feedback(coro,1);
    while (1) {
+     print("presence iterativ\n");
      coro_feedback(coro,2);
    }
    return 0;
-}
+} /*  picture state before and after 'commit' is pressed. */
 
 int corout₋interaction₋histoir(coro_t * coro)
 {
+   print("histoir init\n");
    coro_feedback(coro,1);
    while (1) {
+     print("histoir iterativ\n");
      coro_feedback(coro,2);
    }
    return 0;
-} /* after-commit-rendition. */
-
-struct timeserie angles; version₋ts revision;
+} /*  after-commit rendition. */
 
 int corout₋appendement₋windowcontroller(coro_t * coro)
 {
+   print("angular init\n");
    if (timeserie₋init(&revision,101,5,&angles)) { return -1; }
    if (timeserie₋create(...)) { return -2; }
    coro_feedback(coro,1);
    while (1) {
+     print("angular iterativ\n");
      if (timeserie₋update(...)) { return -3; }
      if (timeserie₋commit(...)) { return -4; }
      coro_feedback(coro,2);
@@ -41,44 +63,108 @@ int corout₋appendement₋windowcontroller(coro_t * coro)
  aktiveras) */
 
 #include <time.h>
-#include <pthread.h>
 #include <dirent.h> /* opendir and readdir. */
 #include <sys/xattr.h> /* getfxattr and fsetxattr. */
 #include <sys/timex.h>
 #include <sys/snapshot.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <termios.h>
+#include <pthread.h>
+#include <stdio.h>
+#include "../Apps/Additions/geolog-orient.h" /* EarthbasedSpatial. */
 
-pthread_t secondary₋thread;
+/*
+ *  keyboard press thread.
+ */
 
-void quit₋program() { pid_t pid=getpid(); kill(pid,SIGQUIT); }
+static pthread_t secondary₋thread;
 
-void interrupt₋program() { pid_t pid=getpid(); kill(pid,SIGINT); }
-
-void begin₋transaction() { /* most likely outcome and assigned guids possibly-maybe stash. */ }
-
-void commit₋transaction(struct Unicodes message) { /* git commit -m '... */ }
-
-int mkdir(char32̄_t * path, ...)
-{ int y;
+inexorable int mkdir(char32̄_t * path, ...) ⓣ
+{ int y1, 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 y2, 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 y3;
    va_prologue(path)
-   int y = Play(path, params, ^(struct Unicodes serial) {
-      int y = TransformAndResolve(path, 
+   y1 = Play(path,__various,^(struct Unicodes serial) {
+      y2 = TransformAndResolve(serial, 
        ^(const char * regular𝘖rLinkpath) {
-          mkdir(regular𝘖rLinkpath, 0777);
+          y3 = mkdir(regular𝘖rLinkpath,0777);
        });
    });
    va_epilogue
+   return y3;
 }
 
-void build₋storage(struct Unicodes base₋project)
-{
-   mkdir(U"⬚/Monitor", ﹟S(base₋project));
-   mkdir(U"⬚/Cloned", ﹟S(base₋project));
-   /* system("git clone https://github.com/andeha/Twinbeam.git"); */
+inexorable int Git(char32̄_t * command, ...)
+{ int y1, 𝑓𝑙𝑢𝑐𝑡𝑢𝑎𝑛𝑡 y2;
+   va_prologue(command)
+   y1 = Play(command, __various, ^(struct Unicodes serial) {
+     char8₋t text[4*serial.tetras]; __builtin_int_t u8bytes;
+     y2 = UnicodeToUtf8(serial.tetras,serial.unicodes,text,&u8bytes);
+     text[u8bytes] = '\0';
+     system((char *)text);
+   });
+   va_epilogue
+   return y2;
 }
+
+void begin₋transaction()
+{
+
+}/*  most likely outcome and assigned guids possibly-maybe stash. */
+
+MENTATIVE commit₋transaction(struct Unicodes noquotes₋message)
+{
+   int y = Git(U"git commit -m '⬚'", ﹟S(noquotes₋message));
+}
+
+MENTATIVE assign₋storage(struct Unicodes base₋project)
+{
+   int y = mkdir(U"⬚/Monitor", ﹟S(base₋project));
+   y = mkdir(U"⬚/Cloned", ﹟S(base₋project));
+}
+
+MENTATIVE nominated₋project(struct Unicodes primaryAndSecondary, 
+ struct Unicodes base₋project)
+{
+   mkdir(U"⬚/Monitor/⬚",﹟S(base₋project),﹟S(primaryAndSecondary));
+}
+
+MENTATIVE
+noninated₋pamplet(
+  struct Unicodes templates, 
+  struct Unicodes primaryAndSecondary, 
+  struct Unicodes base₋project
+)
+{
+   typedef void (^Out)(struct Unicodes);
+   Out out = ^(struct Unicodes unicodes) {
+     
+   };
+   int y = Play(out, U"⬚/Monitor/⬚", base₋project,primaryAndSecondary);
+}
+
+inexorable int augment₋with₋meridians(int fd, struct EarthbasedSpatial 
+ location)
+{ ssize_t error;
+   const char * name₋u8s = "location";
+   const void *material=&location; void * read;
+   ssize_t bytes = sizeof(struct EarthbasedSpatial);
+   error = fgetxattr(fd,name₋u8s,read,bytes,0,0);
+   if (error > 0) {
+     fsetxattr(fd,name₋u8s,material,bytes,0,XATTR_REPLACE);
+   } else {
+     fsetxattr(fd,name₋u8s,material,bytes,0,XATTR_CREATE);
+   }
+   return 0;
+}
+
+MENTATIVE interrupt₋program() { pid_t pid=getpid(); kill(pid,SIGINT); }
+
+MENTATIVE quit₋program() { /* kill(getpid(),SIGQUIT); */ quit=true; }
 
 /* find unicode-symbol in reflection, find fingerprint recollection. */
-/* sometimes unicode-symbol is written in multiple files as experiment and library. */
+/* sometimes unicode-symbol is written in multiple files as experiment and 
+ library. */
 /* see new fingprints assigned after a interaction. */
 /* see difference between snippets retrieved. */
 
@@ -91,10 +177,10 @@ inexorable int wait₋until₋kbhits₋stdio()
 inexorable int get₋stdin₋byte(uint8_t * byte)
 {
    int r = read(0,byte,sizeof(byte));
-   return r < 0 ? -1 : 0;
+   return r<0 ? -1 : 0;
 }
 
-void * input(void *)
+void * input(void * ctxt)
 {
    struct termios tio;
    tcgetattr(STDIN_FILENO,&tio);
@@ -102,11 +188,11 @@ void * input(void *)
    tcsetattr(STDIN_FILENO,TCSANOW,&tio);
    uint8_t sequence[4],byte; int brk=0;
    while (1) {
-     while (!wait₋until₋kbhits₋stdin()) {
-       if (get₋stdin₋byte(&byte)) { print("error"); }
-       if (byte < 128) { print("eight-bit character"); }
+     while (!wait₋until₋kbhits₋stdio()) {
+       if (get₋stdin₋byte(&byte)) { print("error\n"); }
+       if (byte<128) { print("eight-bit character\n"); }
        else {
-         brk += 1; print("multi-byte character");
+         brk+=1; print("multi-byte character\n");
        }
      }
    }
@@ -121,18 +207,22 @@ main(
    coro_t * film = coro_await(corout₋interaction₋presence);
    coro_t * reflection = coro_await(corout₋interaction₋histoir);
    coro_t * interrupt = coro_await(corout₋appendement₋windowcontroller);
-   struct timespec rqtp={0,25000},rmtp={0,0}; /* clock_getres in time.h */
-   if (pthread_create(secondary₋thread,input,ΨΛΩ,ΨΛΩ)) { return -9; }
-   int ts = clock_getres(CLOCK_PROCESS_CPUTIME_ID,&rqtp); /* samma₍n₎stāllning */
+   struct timespec rqtp={0,25000},rmtp={0,0};
+   if (pthread_create(&secondary₋thread,ΨΛΩ,input,ΨΛΩ)) { return -9; }
+   int ts = clock_getres(CLOCK_PROCESS_CPUTIME_ID,&rqtp);
    print("resolution ⬚\n", ﹟d((__builtin_uint_t)ts));
 again:
-   if (nanosleep(&rqtp,rmtp)) { return 1; }
+   if (nanosleep(&rqtp,&rmtp)) { return 1; }
+   if (quit) { goto unagain; }
    coro_resume(film);
    coro_resume(reflection);
    coro_resume(interrupt);
    goto again;
+unagain:
    return 0;
 }
 
-/* compile with xcrun clang -o patent '􀖆 parent-kabinett.c' */
+/*  compile with xcrun clang  -o patent -fmodules-ts -fimplicit-modules      \
+ -fmodule-map-file=../Apps/module.modulemap                                  \
+ -DSHA1GIT=`git log -1 '--pretty=format:%h'` '􀖆 parent-kabinett.c' */
 
