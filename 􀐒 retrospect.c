21294b00d18366691baffc213aaac326feb9f68c
andeha <andeha@users.noreply.github.com>
2022-09-10 19:02:21 +0200

    Improved exposition.

--- 􀐒 retrospect.c
+++ 􀐒 retrospect.c
@@ -1,63 +1,55 @@
-/*  􀆖􀓤 retrospect.c | a language after swift. */
+/*  retros-compi.c | translates for llvm assembly. */
 
 import Twinbeam;
 
 /*
-  
-  import Twinbeam
-  
-  .partial /‌* 'struct' *‌/ after9₋variables
+
+import Twinbeam
+
+  .partial fostrat₋defi|struct after9₋variables
   .end
-  
-  .definite /​* big₋endian₋struct *​/ 4kbframe
-  .end
-  
+
+  .definite big₋endian struct 4kbframe
+  .end /‌/ 'direct definite' equals 'passed as a shallow copy' and 'voluntary definite' equals 'is never null'.
+
   .union Tetra𝘖rUnicode
     int32_t count, char32_t uc
   .end
-  
-  á₋priori To₋precision(half x) -> definite sequent
-  á₋priori 𝟽₋op₋bytereverse(uint8_t b) -> uint8_t
-  
+
+  á₋priori definite /‌* struct *‌/ sequent To₋precision(half x)
+  á₋priori uint8_t 𝟽₋op₋bytereverse(uint8_t b)
+  á₋priori void Base𝕫( __builtin_uint_t|__builtin_int_t|__int128_t|__uint128_t ℤ, unsigned short base, unsigned short digitsOr0, void (^out)(char zeroToNineAndNeg))
   typedef __builtin_int_t Nonabsolute, structa₋middle₋index
-  
-  .compute booth₋multiply, [multiple] multiply
-  .signature (definite sequent x₁, definite sequent x₂) -> definite sequent
-  .additions ACC, mask, X1, X2 as 128 bit signed, hi,lo=0 as int, 
+
+  START(Base𝒛)
+Base𝕫:
+    additions cycle as unsigned short[], k=0 as short;
+    cycle[64] = { 0, ..., 0 }; k=0;
+again₁:
+    cycle[k] = ℕ % base; N /= base; k+=1;
+    if|compare|guard (ℕ) branch|goto again₁;
+    if (digitisOR0) {
+      @<Compute and output each digit>
+    }
+  END(Base𝕫)
+
+  á₋priori definite sequent booth₋multiply(definite sequent x₁, definite sequent x₂)
+  infix binary + definite sequent (definite sequent x1, definite sequent x2) is multiply(x1,x2)
+  .symbol multiply, my₋multiply is booth₋multiply
+
+  START(booth₋multiply)
+booth₋multiply:
+  additions ACC, mask, X1, X2 as 128 bit signed, hi,lo=0 as int, 
     y={ .detail.bits=0, .valid=0 } as definite sequent
-  START(multiply)
-multiply:
 again:
+  END(booth₋multiply)
   
-  compare shift == 127 { }
-  branch again
-  
-  END(multiply)
-  
-  .compute int₋to₋sequent
-  .signature (int64_t ℤ, indirect definite sequent ℝ) -> ΨΛΩ
-  .additions neg as int16_t
+  à₋priori void int₋to₋sequent(int64_t ℤ, indirect definite sequent ℝ)
   START(int₋to₋sequent)
 int₋to₋sequent:
+  additions neg as int16_t
   compare ℤ < 0 { ℤ = -ℤ; LI16 neg, 0; SEH neg }
   END(int₋to₋sequent)
-  
-  .compute normalize_sequent
-  .signature (voluntary definite sequent x₁, voluntary definite sequent x₂) -> ΨΛΩ
-  START(normalize_sequent)
-normalize_sequent:
-  uint64_t top = Modulo(x₂->detail.bits>>64,uint64_t)
-  uint64_t right₋shift₋steps = 64 - __builtin_clzll(top)
-  /‌* { right₋shift₋steps += 1 } when Modulo(x₂->detail.bits,uint64_t) == 0x80000000 *‌/
-  END(normailze_sequent)
-  
-  .compute print₋sequent
-  .signature (int neg, direct definite 𝟽bit₋text integers, direct definite 
-    7bit₋text fracts, void (^zero₋alt₋₋nonused)(), void (^nonvalid)()) -> ΨΛΩ
-  START(print₋sequent)
-print₋sequent:
-  compare 𝕏
-  END(print₋sequent)
  
  */
 
@@ -69,5 +61,3 @@ main(
 {
    return 0;
 }
-
-
