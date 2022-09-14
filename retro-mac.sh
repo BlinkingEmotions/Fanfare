4650740b5a70650432f6e8832f3fbd3a2befd21e
andeha <andeha@users.noreply.github.com>
2022-09-14 22:46:13 +0200

    Improved exposition.

--- retro-mac.sh
+++ retro-mac.sh
@@ -1,35 +1,43 @@
 #!/usr/bin/env /bin/zsh
 
-builtin typeset INITIAL='-g -std=c2x'
+builtin typeset INITIAL='-g -std=c2x -mmacosx-version-min=11.3.1'
 builtin typeset MODULES='-fmodules-ts -fimplicit-modules -fmodule-map-file=🚦.modules'
 builtin typeset MACINTOSH='-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk'
 builtin typeset GIT="-DSHA1GIT=`git log -1 '--pretty=format:%h'`"
-builtin typeset CCARG_MAC='$MODULES $MACINTOSH $GIT'
+builtin typeset CCARG_MAC="$MODULES $MACINTOSH $GIT"
 
 builtin typeset progname=$0
 # builtin typeset -a sku_keys         # array
-builtin typeset -a sku_key
+builtin typeset -a sku_sequence
 builtin typeset debugger
 builtin typeset -A components       # associative a․𝘬․a 'sources'.
 builtin typeset -A output
 
 # sku_keys are retros-compi, c-maskin, turbin, parent-kabinett, intel-hex, uql and friends.
 
-components["retros-compi"]='􀐒 retros-compi.c'
-components["c-maskin"]='􀖆 ⒞-maskin.c'
-components["turbin"]='􀖆 turbin.cpp'
-components["parent-kabinett"]='􀖆 parent-kabinett.c'
-components["intel-hex"]='🥽 Intelhex.cpp'
-components["uql-gen"]='🥽⋆UQL.cpp 🥽⋆UQL₂.cpp'
-components["enforce-beskow"]='􁕊 enforce-beskow.c'
+components=(
+  'retros-compi' '􀐒 retros-compi.c'
+  'c-maskin' '􀖆 ⒞-maskin.c'
+  'turbin' '􀖆 turbin.cpp'
+  'parent-kabinett' '􀖆 parent-kabinett.c'
+  'intel-hex' '🥽 Intelhex.cpp'
+  'uql-gen' '🥽⋆UQL.cpp 🥽⋆UQL₂.cpp'
+  'linguistics-epi' '􀥳 lingustics-epi.c'
+  'enforce-beskow' '􁕊 enforce-beskow.c'
+  'nut-f1-schweiz' '􀐒 slow-pid.c'
+)
 
-output["retros-compi"]="run-link"
-output["c-maskin"]="helixsh" 
-output["turbin"]="turbin"
-output["parent-kabinett"]="patent"
-output["intel-hex"]="intelhex"
-output["uql-gen"]="question"
-output["enforce-beskow"]='beskow'
+output=(
+  'retros-compi' 'run-link'
+  'c-maskin' 'helixsh'
+  'turbin' 'turbin'
+  'parent-kabinett' 'patent'
+  'intel-hex' 'intelhex'
+  'uql-gen' "question"
+  'linguistics-epi' 'linguistics-epi'
+  'enforce-beskow' 'beskow'
+  'nut-f1-schweiz' 'non-bell'
+)
 
 function usage
 {
@@ -65,9 +73,10 @@ while [[ $# -gt 0 ]]; do
      ;;
    -help | --help | -h | --h | -\? )
      usage
+     builtin bye 1
      ;;
    * )
-     sku_key+="$1"
+     sku_sequence+="$1"
      builtin shift
      ;;
   esac
@@ -75,11 +84,13 @@ done
 
 function compile_and_run()
 {
-   command xcrun clang $INITIAL -o $output[$sku_key] $CCARG_MAC $components[$sku_key]
-   if [[ -n "$debugger" ]]; then
-     command xcrun lldb $output[$sku_key]
-   fi
+   for sku_key ($sku_sequence) {
+     builtin typeset text="-o $output[$sku_key] ${CCARG_MAC}"
+     builtin command xcrun "clang $INITIAL $text '$components[$sku_key]'"
+     if [[ -n "$debugger" ]]; then
+       command xcrun lldb $output[$sku_key]
+     fi
+   }
 }
 
 compile_and_run
-
