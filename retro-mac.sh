dd5bbd9885fd787b82df6699817712f8301aa1a9
andeha <andeha@users.noreply.github.com>
2022-09-14 14:56:24 +0200

    Improved exposition.

--- retro-mac.sh
+++ retro-mac.sh
@@ -3,12 +3,12 @@
 builtin typeset INITIAL='-g -std=c2x'
 builtin typeset MODULES='-fmodules-ts -fimplicit-modules -fmodule-map-file=🚦.modules'
 builtin typeset MACINTOSH='-isysroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk'
-builtin typeset GIT='-DSHA1GIT=`git log -1 '--pretty=format:%h'`'
+builtin typeset GIT="-DSHA1GIT=`git log -1 '--pretty=format:%h'`"
 builtin typeset CCARG_MAC='$MODULES $MACINTOSH $GIT'
 
 builtin typeset progname=$0
 # builtin typeset -a sku_keys         # array
-builtin typeset sku_key=$1
+builtin typeset -a sku_key
 builtin typeset debugger
 builtin typeset -A components       # associative a․𝘬․a 'sources'.
 builtin typeset -A output
@@ -20,8 +20,7 @@ components["c-maskin"]='􀖆 ⒞-maskin.c'
 components["turbin"]='􀖆 turbin.cpp'
 components["parent-kabinett"]='􀖆 parent-kabinett.c'
 components["intel-hex"]='🥽 Intelhex.cpp'
-components["uql"]='🥽⋆UQL.cpp 🥽⋆UQL₂.cpp'
-components["linguistics-epi"]='􀥳 lingustics-epi.c'
+components["uql-gen"]='🥽⋆UQL.cpp 🥽⋆UQL₂.cpp'
 components["enforce-beskow"]='􁕊 enforce-beskow.c'
 
 output["retros-compi"]="run-link"
@@ -29,13 +28,12 @@ output["c-maskin"]="helixsh"
 output["turbin"]="turbin"
 output["parent-kabinett"]="patent"
 output["intel-hex"]="intelhex"
-output["uql"]="question"
-output["linguistics-epi"]='linguistics-epi'
+output["uql-gen"]="question"
 output["enforce-beskow"]='beskow'
 
 function usage
 {
-   cat << HEREDOC
+   command cat << HEREDOC
    
    Usage $progname [-d] SKU-key
    
@@ -44,44 +42,44 @@ function usage
      -d, --debug         start debugger after compilation.
    
    Sku-keys:
-
+   
 HEREDOC
    
    for key value in ${(kv)components}; do
-     echo "$key: $value"
+     builtin echo "$key: $value"
    done
    
-   echo " "
+   builtin printf "\n"
 }
 
 if [[ $# -eq 0 ]]; then
   usage
-  exit 2
+  builtin bye 1
 fi
 
 while [[ $# -gt 0 ]]; do
   case $1 in 
    -d | --debugger)
-     shift
+     builtin shift
      debugger="yes"
      ;;
    -help | --help | -h | --h | -\? )
      usage
-     exit 1
      ;;
    * )
-     skus+="$1"
-     shift
+     sku_key+="$1"
+     builtin shift
      ;;
   esac
 done
 
 function compile_and_run()
 {
-   xcrun clang $INITIAL -o $output[$sku_key] $CCARG_MAC $components[$sku_key]
+   command xcrun clang $INITIAL -o $output[$sku_key] $CCARG_MAC $components[$sku_key]
    if [[ -n "$debugger" ]]; then
-   	 xcrun lldb $output[$sku_key]
+     command xcrun lldb $output[$sku_key]
    fi
 }
 
 compile_and_run
+
