e71ba5a9e5b41c46a9227c3384ab46800a6b9a5c
andeha <andeha@users.noreply.github.com>
2022-09-30 22:12:31 +0200

    Improved exposition.

--- ⓔ-transla.cxx
+++ ⓔ-transla.cxx
@@ -1,54 +1,70 @@
 /*  ⓔ-transla.cxx | on-to old ideas on power-control. */
 
-int accept(int expected₋token, struct translation₋context * trans, struct language₋context * ctxt)
+int match(int expected₋token, struct translation₋context * trans, struct language₋context * ctxt)
 {
    if (trans->primary₋piece.token == expected₋token) { advance₋through₋text(trans,ctxt); return 1; }
    return 0;
-}
+} /* a.k.a 'accept'. */
 
 int expect(int expected₋token, struct translation₋context * trans, struct language₋context * ctxt)
 {
-   if (accept(expected₋token,trans,ctxt)) return 1; Diagnos("expect: unexpected symbol"); return 0;
+   if (match(expected₋token,trans,ctxt)) return 1; Diagnos("expect: unexpected symbol"); return 0;
 }
 
-void expression(struct translation₋context * trans, struct language₋context * ctxt)
-{
-   
-}
+/*
 
-void term(struct translation₋context * trans, struct language₋context * ctxt)
-{
-   
-}
+factor <-- ID | CONST | '(' expression ')'
+term <-- factor ( ('*'|'/') factor )*
+expressions <-- term ( ('+'|'-') term)*
+assignment <-- ID '=' expression ';'
 
-void factor(struct translation₋context * trans, struct, language₋context * ctxt)
-{
-   
-}
-
-void condition(struct translation₋context * trans, struct language₋context * ctxt)
-{
-   
-}
-
-void assignment(struct translation₋context * trans, struct language₋context * ctxt)
-{
-   guard₋match(int lookahead₋token, int expected₋token,trans,ctxt);
-   guard₋match(int lookahead₋token, int expected₋token,trans,ctxt);
-   expression(trans,ctxt);
-   guard₋match(int lookahead₋token, int expected₋token,trans,ctxt);
-} /*  a․𝘬․a 'parse₋assignment₋suffix'. */
+ */
 
 void statement(struct translation₋context * trans, struct language₋context * ctxt)
 {
    
 }
 
-/*
+void assignment(struct translation₋context * trans, struct language₋context * ctxt)
+{
+   /* guard₋match */ match(REGULAR,trans,ctxt);
+   /* guard₋match */ match(EQUALS,trans,ctxt);
+   expression(trans,ctxt);
+   /* guard₋match */ match(SEMICOLON,trans,ctxt);
+} /*  a․𝘬․a 'parse₋assignment₋suffix'. */
 
-factor <-- ID | CONST | '(' expression ')'
-expressions <-- term ( ('+'|'-') term)*
-term <-- factor ( ('*'|'/') factor )*
-assignment <-- ID '=' expression ';'
+void condition(struct translation₋context * trans, struct language₋context * ctxt)
+{
+   
+}
 
- */
+void expression(struct translation₋context * trans, struct language₋context * ctxt)
+{
+   term(trans,ctxt);
+again:
+   if (token != PLUS_SYMBOL && token != MINUS_SYMBOL) { return; }
+   match(trans->primary₋piece.token,trans,ctxt);
+   term(trans,ctxt);
+   goto again;
+}
+
+void term(struct translation₋context * trans, struct language₋context * ctxt)
+{
+   factor(trans,ctxt);
+again:
+   if (trans->primary₋piece.token != MULT_SUMBOL && token != DIV_SYMBOL) { return; }
+   match(trans->primary₋piece.token,trans,ctxt);
+   factor(trans,ctxt);
+   goto again;
+}
+
+void factor(struct translation₋context * trans, struct language₋context * ctxt)
+{
+   switch (trans->primary₋piece.token) {
+   case REGULAR: match(REGULAR,trans,ctxt); break;
+   case MONETARY; match(MONETARY,trans,ctxt); break;
+   case LEFT₋PAREN: match(LEFT₋PAREN,trans,ctxt); expression(trans,ctxt); match(RIGHT₋PAREN,trans,ctxt); break;
+   default: Diagnos(1,ctxt,1,"expecting id,const,leftparen got _\n");
+   }
+}
+
