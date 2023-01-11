c9489e431fbd6326cf980c0c46f8b49b17009e2e
andeha <andeha@users.noreply.github.com>
2023-01-11 08:44:08 +0100

    Improved exposition.

---
+++ ╳-runlink-symbols.cxx
@@ -0,0 +1,11 @@
+/*  ╳-runlink-symbols.cxx - internal references used inside machine translation. */
+
+enum symbol₋class { ident=1, number, preproc₋include, preproc₋if, 
+ preproc₋end, preproc₋defined, diffusesym, importsym, partialsym, fostratdefisym, structsym, 
+ end₋with₋dotsym, definitesym, big₋endiansym, little₋endiansym, 
+ unionsym, apriorisym, typedefsym, constantsym, computesym, comparesym, 
+ ifsym, gotosym, transcriptsym, inexorablesym, mentativesym, startsym, 
+ inlinesym, coroutinesym, endsym, additionssym, assym , indirectsym, 
+ voluntarysym, intsym, char8₋tsym, char32̄_tsym, binary32sym, decimal32sym, 
+ tertary32sym, unsignedsym, schemasym, end₋of₋transmission₋and₋file };
+
