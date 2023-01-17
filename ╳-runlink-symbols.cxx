6edc55c61af8e03b3ea3302b8f3f5f398a181754
andeha <andeha@users.noreply.github.com>
2023-01-17 08:03:23 +0100

    Improved exposition.

--- ╳-runlink-symbols.cxx
+++ ╳-runlink-symbols.cxx
@@ -1,11 +1,12 @@
 /*  ╳-runlink-symbols.cxx - internal references used inside machine translation. */
 
-enum symbol₋class { ident=1, number, preproc₋include, preproc₋if, 
- preproc₋end, preproc₋defined, diffusesym, importsym, partialsym, fostratdefisym, structsym, 
- end₋with₋dotsym, definitesym, big₋endiansym, little₋endiansym, 
- unionsym, apriorisym, typedefsym, constantsym, computesym, comparesym, 
- ifsym, gotosym, transcriptsym, inexorablesym, mentativesym, startsym, 
- inlinesym, coroutinesym, endsym, additionssym, assym , indirectsym, 
- voluntarysym, intsym, char8₋tsym, char32̄_tsym, binary32sym, decimal32sym, 
- tertary32sym, unsignedsym, schemasym, end₋of₋transmission₋and₋file };
+enum symbol₋class { diffusesym, preproc₋if, preproc₋else, preproc₋elif, 
+ preproc₋end, preproc₋include, preproc₋defined, preproc₋define, 
+ partialsym, fostratdefisym, structsym, definitesym, end₋and₋dotsym, 
+ big₋endiansym, little₋endiansym, unionsym, apriorisym, typedefsym, 
+ constantsym, computesym, collide, comparesym, ifsym, gotosym, transcriptsym, 
+ inexorablesym, mentativesym, startsym, inlinesym, coroutinesym, endsym, 
+ additionssym, ident, number, assym, indirectsym, voluntarysym, 
+ intsym, char8₋tsym, char32̄_tsym, binary32sym, decimal32sym, tertary32sym, 
+ unsignedsym, schemasym, end₋of₋transmission₋and₋file };
 
