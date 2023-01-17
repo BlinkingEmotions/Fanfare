de04b62b233bff4aa2403eb207cf54c09dd364f9
andeha <andeha@users.noreply.github.com>
2023-01-17 09:04:59 +0100

    Improved exposition.

--- ╳-runlink-symbols.cxx
+++ ╳-runlink-symbols.cxx
@@ -6,7 +6,8 @@ enum symbol₋class { diffusesym, preproc₋if, preproc₋else, preproc₋elif,
  big₋endiansym, little₋endiansym, unionsym, apriorisym, typedefsym, 
  constantsym, computesym, collide, comparesym, ifsym, gotosym, transcriptsym, 
  inexorablesym, mentativesym, startsym, inlinesym, coroutinesym, endsym, 
- additionssym, ident, number, assym, indirectsym, voluntarysym, 
+ additionssym, ident, number, assym, indirectsym, voluntarysym, isomorphsym, 
  intsym, char8₋tsym, char32̄_tsym, binary32sym, decimal32sym, tertary32sym, 
  unsignedsym, schemasym, end₋of₋transmission₋and₋file };
+
 
