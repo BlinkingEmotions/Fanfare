573fd01952482ab9fbcd32247909f044fb824fe3
andeha <andeha@users.noreply.github.com>
2023-01-21 07:01:40 +0100

    Improved exposition.

--- µ⃝-code-and-tree.cxx
+++ µ⃝-code-and-tree.cxx
@@ -59,42 +59,61 @@ struct dynamic₋bag * new₋Function(Nonabsolut symbol, struct dynamic₋bag * 
    return node;
 }
 
-int indentation=0; Argᴾ ﹟run(Nonabsolut);
+int indentation=0; Argᴾ ﹟ident(Nonabsolut);
+
+Argᴾ ﹟r(struct dynamic₋bag * item)
+{
+   return ﹟d((__builtin_uint_t)item);
+}
+
+void print₋datatree(int brk, struct dynamic₋bag * item)
+{ int i=0;
+again:
+   if (i>=brk) { goto unagain; }
+   print("⬚: ⬚ ⬚ ⬚ ⬚ ⬚ ⬚ ⬚ ⬚ ⬚ ⬚ ⬚ and ⬚\n", ﹟d((__builtin_uint_t)item),﹟d(item->T),﹟r(item->l),﹟r(item->r),
+    ﹟r(item->compare₋then),﹟r(item->compare₋else),﹟r(item->sequence),
+    ﹟r(item->element),﹟r(item->next),﹟r(item->prev),﹟r(item->formal),﹟r(item->detail), ﹟d(item->X.store.regularOrIdent));
+   i+=1; goto again;
+unagain:
+   ;
+}
 
 void print₋tree(struct dynamic₋bag * item)
 {
    typedef void (^Print)(char *);
    typedef void (^Detail)(struct dynamic₋bag * item);
    typedef void (^Every)(struct dynamic₋bag *, Detail);
-   Print trace = ^(char * operation) { print("⬚ @⬚\n",﹟s7(operation), 
-    ﹟d((__builtin_int_t)item->memory)); indentation+=1; 
-    print₋tree(item->l); print₋tree(item->r); indentation+=-1; };
+
+   Print infix = ^(char * operation) { for (int i=0; i<indentation; i+=1) { print(" "); } 
+    indentation+=1; print₋tree(item->l); indentation+=-1;
+    print("⬚ @⬚\n",﹟s7(operation),﹟d((__builtin_int_t)item->memory)); indentation+=2; 
+    print₋tree(item->r); indentation+=-1; };
    Every each = ^(struct dynamic₋bag * item₋first, Detail detail) { 
     struct dynamic₋bag * i=item₋first; for (;i;i=i->next) { detail(i); } };
    Detail detail = ^(struct dynamic₋bag * item) { print₋tree(item); };
    switch (item->T)
    {
-   case ident: print("ident '⬚' @⬚\n", ﹟run(item->X.store.regularOrIdent),﹟d((__builtin_int_t)item->memory)); break;
+   case ident: print("ident '⬚' @⬚\n", ﹟ident(item->X.store.regularOrIdent),﹟d((__builtin_int_t)item->memory)); break;
    case number: print("number '⬚' @⬚\n", ﹟d(item->X.store.integer), ﹟d((__builtin_int_t)item->memory)); break;
-   case times: trace("times"); break;
-   case divide: trace("divide"); break;
-   case plus: trace("plus"); break;
-   case minus: trace("minus"); break;
-   case eql: trace("eql"); break;
-   case neq: trace("neq"); break;
-   case lss: trace("lss"); break;
-   case leq: trace("leq"); break;
-   case gtr: trace("gtr"); break;
-   case geq: trace("geq"); break;
-   case callsym: print("call '⬚'\n", ﹟run(item->X.store.regularOrIdent)); break;
-   case branch₋goto₋optsym: print("branch ⬚\n", ﹟run(item->X.store.regularOrIdent)); break;
+   case times: infix("times"); break;
+   case divide: infix("divide"); break;
+   case plus: infix("plus"); break;
+   case minus: infix("minus"); break;
+   case eql: infix("eql"); break;
+   case neq: infix("neq"); break;
+   case lss: infix("lss"); break;
+   case leq: infix("leq"); break;
+   case gtr: infix("gtr"); break;
+   case geq: infix("geq"); break;
+   case callsym: print("call '⬚'\n", ﹟ident(item->X.store.regularOrIdent)); break;
+   case branch₋goto₋optsym: print("branch ⬚\n", ﹟ident(item->X.store.regularOrIdent)); break;
    case ifsym: print("compare\n"); indentation+=1; print₋tree(item->compare₋then); 
     print₋tree(item->compare₋else); indentation+=-1; break;
    case afterward: print("afterward @⬚\n", ﹟d((__builtin_int_t)item->memory)); indentation+=1; 
     print₋tree(item->l); if (item->r) print₋tree(item->r); else print("<unassigned>\n"); break;
    case constsym: print("constsym\n"); each(item->sequence,detail); break;
    case varsym: print("varsym\n"); each(item->sequence,detail); break;
-   case procsym: print("procsym '⬚'\n", ﹟run(item->X.store.regularOrIdent)); 
+   case procsym: print("procsym '⬚'\n", ﹟ident(item->X.store.regularOrIdent)); 
     indentation+=1; each(item->formal,detail); print("\n"); 
     each(item->detail,detail); indentation+=-1; break;
    default: { print("unknown item with symbol-type '⬚'\n", ﹟d((__builtin_int_t)item->T)); }
