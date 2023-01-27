f85190d2cb3f704bdf125541582c6b9807689d80
andeha <andeha@users.noreply.github.com>
2023-01-27 09:09:56 +0100

    Improved exposition.

---
+++ ∎|∎ radbanc.c
@@ -0,0 +1,21 @@
+/*  radbanc.c | linked-list and no reorganization allowed. */
+
+struct oval₋tree { unicode₋shatter name; };
+
+struct necklace { struct oval₋tree * materialºª,last; } left₋hand;
+
+int necklace₋init() { }
+
+int necklace₋uninit() { }
+
+int append₋at₋end() { }
+
+int unqueue() { }
+
+int rollback() { }
+
+int main()
+{
+   left₋hand.materialºª = Cons₋alloc(sizeof(struct oval₋tree));
+}
+
