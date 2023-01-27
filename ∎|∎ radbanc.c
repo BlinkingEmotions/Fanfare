4976f1dce1762a29d5c898a75f8246b113fc8b2a
andeha <andeha@users.noreply.github.com>
2023-01-27 16:31:27 +0100

    Improved exposition.

--- ∎|∎ radbanc.c
+++ ∎|∎ radbanc.c
@@ -1,8 +1,16 @@
 /*  radbanc.c | linked-list and no reorganization allowed. */
 
+import Twinbeam;
+
 struct oval₋tree { unicode₋shatter name; };
 
-struct necklace { struct oval₋tree * materialºª,last; } left₋hand;
+union oval₋tree₋continuation { struct oval₋tree * next; __builtin_uint_t possibly₋maybe; };
+
+struct necklace { struct oval₋tree₋continuation * materialºª,last; } left₋hand;
+
+
+struct oval₋tree₋cons { struct oval₋tree item; union oval₋tree₋continuation nxt; };
+/*  non-'circular' therefore single-linked. */
 
 int necklace₋init() { }
 
@@ -17,5 +25,12 @@ int rollback() { }
 int main()
 {
    left₋hand.materialºª = Cons₋alloc(sizeof(struct oval₋tree));
+   if (append₋at₋end()) { return 1; }
+   if (append₋at₋end()) { return 2; }
+   if (unqueue()) { return 3; }
+   if (rollback()) { return 4; }
+   return 0;
 }
+
+/* \also 'sys/queue.h'. */
 
