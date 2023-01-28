6f368ff4096da0c5f296c10207b3c4391c8db30e
andeha <andeha@users.noreply.github.com>
2023-01-28 05:19:48 +0100

    Improved exposition.

--- ∎|∎ radbanc.c
+++ ∎|∎ radbanc.c
@@ -11,23 +11,29 @@ struct oval₋tree₋cons { struct oval₋tree item; union oval₋tree₋continu
 
 struct necklace { struct oval₋tree₋cons * materialºª,last; } left₋hand;
 
-int necklace₋init(int count, void (^init)(int count, struct oval₋tree ** sometime)) { }
+int necklace₋init(int count, void (^init)(int count, struct oval₋tree ** 
+ uninited₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * last) 
+{
+   left₋hand.materialºª = Cons₋alloc(sizeof(struct oval₋tree));
+   return 0;
+}
 
-int necklace₋uninit(void (^uninit)(int count, struct oval₋tree ** sometime)) { }
+int necklace₋uninit(void (^uninit)(int count, struct oval₋tree ** snapshot₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * last) { }
 
-int append₋at₋end() { }
+int append₋at₋end(int count, void (^augment)(int count, struct oval₋tree ** uninited₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * last) { }
 
-int unqueue() { }
+int unqueue(int count, void (^removed)(int count, struct oval₋tree ** snapshot₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * last) { }
 
-int rollback() { }
+int rollback₋pop(void (^scalar)(struct oval₋tree * snapshot₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * last) { }
 
 int main()
 {
-   left₋hand.materialºª = Cons₋alloc(sizeof(struct oval₋tree));
-   if (append₋at₋end()) { return 1; }
-   if (append₋at₋end()) { return 2; }
-   if (unqueue()) { return 3; }
-   if (rollback()) { return 4; }
+   if (necklace₋init(0,0,left₋hand.materialºª,left₋hand.last)) { return 1; }
+   if (append₋at₋end(1,0,left₋hand.materialºª,left₋hand.last)) { return 2; }
+   if (append₋at₋end(1,0,left₋hand.materialºª,left₋hand.last)) { return 3; }
+   if (unqueue(1,0,left₋hand.materialºª,left₋hand.last)) { return 4; }
+   if (rollback₋pop(0,left₋hand.materialºª,left₋hand.last)) { return 5; }
+   if (necklace₋uninit(0,left₋hand.materialºª,left₋hand.last)) { return 7; }
    return 0;
 }
 
