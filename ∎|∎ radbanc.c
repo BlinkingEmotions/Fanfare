260786f0439dbd0891ac906055eb3156015573df
andeha <andeha@users.noreply.github.com>
2023-01-31 04:07:44 +0100

    Improved exposition.

--- ∎|∎ radbanc.c
+++ ∎|∎ radbanc.c
@@ -9,25 +9,40 @@ union oval₋tree₋continuation { struct oval₋tree * next; __builtin_uint_t p
 struct oval₋tree₋cons { struct oval₋tree item; union oval₋tree₋continuation nxt; };
 /*  non-'circular' therefore single-linked. */
 
+int rollback₋pop(void (^)(struct oval₋tree *));
+int unqueue(int count, void (^)(int, struct oval₋tree **), struct oval₋tree **, struct oval₋tree **);
+int append₋at₋end(int, void (^augment)(int, struct oval₋tree **), struct oval₋tree ** struct oval₋tree **);
+int is₋empty(struct oval₋tree **, struct oval₋tree **);
+
 struct necklace { struct oval₋tree₋cons * materialºª,last; } left₋hand;
 
 int necklace₋init(int count, void (^init)(int count, struct oval₋tree ** 
- uninited₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * last)
-{ int i=0; struct oval₋tree₋cons * current;
-   first = last = 0;
+ uninited₋sometime), struct necklace * 🅷)
+{ int i=0; struct oval₋tree₋cons * current; /* curl, div and rot. */
+   🅷->materialºª = 🅷->last = 0; struct oval₋tree * collect[count];
 again:
-   if (i >= count) { return 0; }
+   if (i >= count) { goto unagain; }
    current = Cons₋alloc(sizeof(struct oval₋tree));
    current->nxt = first;
    first = current;
-   i +=1; goto again;
+   i+=1; goto again;
+unagain:
+   init(count,collect);
+   return 0;
 }
 
 int necklace₋uninit(void (^uninit)(int count, struct oval₋tree ** 
- snapshot₋sometime), struct oval₋tree₋cons * first, struct oval₋tree₋cons * 
- last)
+ snapshot₋sometime), struct necklace * 🅷)
 { 
- 
+   if (unqueue(1,^(int count, struct oval₋tree ** snapshot₋sometime) { 
+     uninit(count,snapshot₋sometime);
+   },&🅷->materialºª, &🅷->last)) { return -1; }
+   return 0;
+}
+
+int is₋empty(struct oval₋tree * first, struct oval₋tree * last)
+{
+   return first == 0 && last == 0;
 }
 
 int append₋at₋end(int count, void (^augment)(int count, struct oval₋tree ** 
@@ -48,15 +63,28 @@ unagain:
 } /* 'Ordo ett' */
 
 int unqueue(int count, void (^removed)(int count, struct oval₋tree ** 
- snapshot₋sometime), struct oval₋tree₋cons * first, struct 
- oval₋tree₋cons * last)
+ snapshot₋sometime), struct oval₋tree₋cons ** first, struct 
+ oval₋tree₋cons ** last)
 { int i=0; struct oval₋tree₋cons *collecta[count];
 again:
    if (i >= count) { goto unagain; }
+   collecta[i] = *first;
+   *first=first->next;
+   i+=1; goto again;
+unagain:
+   removed(i,collecta);
 }
 
 int rollback₋pop(void (^scalar)(struct oval₋tree * snapshot₋sometime), struct 
- oval₋tree₋cons * first, struct oval₋tree₋cons * last) { }
+ oval₋tree₋cons ** first, struct oval₋tree₋cons ** last)
+{ oval₋tree₋cons * iter = *first;
+   if (iter == 0) { return -1; }
+   if (iter->nxt == 0) { * return 0; }
+again:
+   if (iter->nxt == last) { return 0; }
+   iter = iter->nxt;
+   goto again;
+}
 
 int main()
 {
@@ -69,4 +97,10 @@ int main()
    return 0;
 }
 
-/* \also 'sys/queue.h'. */
+/* compile with 
+ 
+  clang -g -fmodules-ts -fimplicit-modules -fmodule-map-file=🚦.modules      \
+   '∎|∎ radbanc.c' ../Apps/Source/Releases/libTwinbeam-x86_64.a
+ 
+ \also 'sys/queue.h'. */
+
