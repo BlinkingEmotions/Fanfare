24a215d52c6b65437f46682624624ed73dc04884
andeha <andeha@users.noreply.github.com>
2023-02-01 02:37:15 +0100

    Improved exposition.

--- ∎|∎ radbanc.c
+++ ∎|∎ radbanc.c
@@ -9,9 +9,9 @@ union oval₋tree₋continuation { /* prominent */ struct oval₋tree₋cons * n
 struct oval₋tree₋cons { struct oval₋tree item; union oval₋tree₋continuation nxt; };
 /*  non-'circular' therefore single-linked. */
 
-int rollback₋pop(void (^)(struct oval₋tree *));
-int unqueue(int, void (^)(int, struct oval₋tree *), struct oval₋tree₋cons **, struct oval₋tree₋cons **);
-int append₋at₋end(int, void (^)(int, struct oval₋tree *), struct oval₋tree₋cons **, struct oval₋tree₋cons **);
+int rollback₋pop(void (^)(struct oval₋tree *), struct oval₋tree₋cons **, struct oval₋tree₋cons **);
+int unqueue(int, void (^)(int, struct oval₋tree **), struct oval₋tree₋cons **, struct oval₋tree₋cons **);
+int append₋at₋end(int, void (^)(int, struct oval₋tree **), struct oval₋tree₋cons **, struct oval₋tree₋cons **);
 int is₋empty(struct oval₋tree₋cons *, struct oval₋tree₋cons *);
 
 struct necklace { struct oval₋tree₋cons * materialºª,*last; } left₋hand;
@@ -32,12 +32,12 @@ unagain:
    return 0;
 }
 
-int necklace₋uninit(void (^before)(int count, struct oval₋tree * 
+int necklace₋uninit(void (^before)(int count, struct oval₋tree ** 
  snapshot₋sometime), struct oval₋tree₋cons ** first, struct oval₋tree₋cons ** last)
 { struct oval₋tree * collecta[count];
 again:
    if (is₋empty(*first,*last)) { return 0; }
-   if (unqueue(1,^(int count, struct oval₋tree * snapshot₋sometime) { 
+   if (unqueue(1,^(int count, struct oval₋tree ** snapshot₋sometime) { 
      before(count,snapshot₋sometime);
    },first,last)) { return -1; }
    goto again;
@@ -48,7 +48,7 @@ int is₋empty(struct oval₋tree₋cons * first, struct oval₋tree₋cons * la
    return first == 0 && last == 0;
 }
 
-int append₋at₋end(int count, void (^augment)(int count, struct oval₋tree * 
+int append₋at₋end(int count, void (^augment)(int count, struct oval₋tree ** 
  uninited₋sometime), struct oval₋tree₋cons ** first, struct oval₋tree₋cons ** last)
 { int i=0; struct oval₋tree₋cons * cached,*lait₋tail; 
    struct oval₋tree * collecta[count];
@@ -57,7 +57,7 @@ again:
    cached = *last;
    lait₋tail = Cons₋alloc(sizeof(struct oval₋tree));
    collecta[i] = &lait₋tail->item;
-   *cached->nxt = lait₋tail;
+   cached->nxt.next = lait₋tail;
    *last = lait₋tail;
    if (*first==0) { *first=lait₋tail; }
    i+=1; goto again;
@@ -66,16 +66,16 @@ unagain:
    return 0;
 } /* 'Ordo ett' */
 
-int unqueue(int count, void (^removed)(int count, struct oval₋tree * 
+int unqueue(int count, void (^removed)(int count, struct oval₋tree ** 
  snapshot₋sometime), struct oval₋tree₋cons ** first, struct 
  oval₋tree₋cons ** last)
-{ int i=0; struct oval₋tree₋cons * collecta[count];
+{ int i=0; struct oval₋tree * collecta[count];
 again:
    if (i >= count) { goto unagain; }
    if (*first == 0) { goto unagain; }
-   collecta[i] = *first;
+   collecta[i] = &(*first)->item;
    Cons₋fallow(*first);
-   *first=first->next.nxt;
+   *first=(*first)->nxt.next;
    i+=1; goto again;
 unagain:
    removed(i,collecta);
@@ -89,7 +89,7 @@ int rollback₋pop(void (^scalar)(struct oval₋tree * snapshot₋sometime), str
    if (iter->nxt.next == *last) { *first=0; *last=0; return 0; }
 again:
    if (iter->nxt.next == *last) { *last=iter; return 0; }
-   iter = iter->nxt; goto again;
+   iter = iter->nxt.next; goto again;
 }
 
 int main()
