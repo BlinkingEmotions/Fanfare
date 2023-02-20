a0b8b3ffc90f536c577be69324465a8327aaa062
andeha <andeha@users.noreply.github.com>
2023-02-20 01:28:31 +0100

    Improved exposition.

--- µ⃝-syntax-pretty.cxx
+++ µ⃝-syntax-pretty.cxx
@@ -1,74 +1,102 @@
 /*  µ⃝-syntax-pretty.cxx | prints abstract syntax tree from left to right. */
 
-void stmt₋print(struct tabcontext * tabs, struct dynamic₋bag * statement)
-{
-   switch (statement->T)
-   {
-   case callsym: print("call '⬚'\n",﹟ident(statement->X.store.regular)); break;
-   case branch₋goto₋optsym: print("branch to '⬚'\n",﹟ident(statement->X.store.regular)); break;
-   case ifsym: print("compare\n"); infix₋print(); stmt₋print(); stmt₋print(); break;
-   case afterward: print("assign\n"); break;
-   default: print("<unrecognized statement>\n");
-   }
-}
-
-void unary₋print(struct tabcontext * tabs, char * op, struct dynamic₋bag * expression)
-{
-   switch (expression->T)
+void print₋expr(struct tabcontext * tabs, struct dynamic₋bag * expr)
+{ char * op;
+   switch (expr->T)
    {
    case ident: print("⬚ident '⬚' @⬚\n",﹟indent(0,tabs), 
-    ﹟ident(item->X.store.regular),﹟short(item->memory)); break;
+    ﹟ident(expr->X.store.regular),﹟short(expr->memory)); return;
    case number: print("⬚number '⬚' @⬚\n",﹟indent(0,tabs), 
-    ﹟d(item->X.store.integer),﹟short(item->memory)); break;
+    ﹟d(expr->X.store.integer),﹟short(expr->memory)); return;
+   case times: op="*"; goto tail;
+   case divide: op="÷"; goto tail;
+   case plus: op="+"; goto tail;
+   case minus: op="-"; goto tail;
+   case eql: op="="; goto tail;
+   case neq: op="≠"; goto tail;
+   case lss: op="<"; goto tail;
+   case leq: op="≤"; goto tail;
+   case gtr: op=">"; goto tail;
+   case geq: op="≥"; goto tail;
+   default: print("unknown expression and symbol-type '⬚'\n", ﹟d(expr->T)); return;
    }
+tail:
+   print₋expr(tabs,expr->form.l);
+   print("⬚⬚ @⬚\n",﹟indent(0,tabs),﹟s7(op),﹟short(expr->memory));
+   print₋expr(tabs,expr->form.r);
 }
 
-void infix₋print(struct tabcontext * tabs, char * op, struct dynamic₋bag * expression)
-{
-   print₋tree(tabs,expression->form.l);
-   switch (expression->T)
-   {
-   case times: infix₋print(tabs,"times"); break;
-   case divide: infix₋print(tabs,"÷"); break;
-   case plus: infix₋print(tabs,"+"); break;
-   case minus: infix₋print(tabs,"-"); break;
-   case eql: infix₋print(tabs,"="); break;
-   case neq: infix₋print(tabs,"≠"); break;
-   case lss: infix₋print(tabs,"<"); break;
-   case leq: infix₋print(tabs,"≤"); break;
-   case gtr: infix₋print(tabs,">"); break;
-   case geq: infix₋print(tabs,"≥"); break;
-   }
-   print("⬚⬚ @⬚\n",﹟indent(0,tabs),﹟s7(op),﹟short(expression->memory));
-   print₋tree(tabs,expression->form.r);
-}
-
-void list₋print(struct dynamic₋bag₋cons * list₋first, void (^elem)(struct dynamic₋bag * item))
+void list₋print(struct dynamic₋bag₋cons * list₋first, 
+ void (^element)(struct dynamic₋bag * item))
 { struct dynamic₋bag₋cons * memory = list₋first;
    print("[");
 again:
    if (memory == 0) goto unagain;
-   elem(memory->item);
+   element(memory->item);
    memory = memory->nxt.next; goto again;
 unagain:
    print("]");
 }
 
-void print₋ast(struct tabcontext * tabs, struct dynamic₋bag₋cons * machine₋recollect)
-{
-   switch (item->T)
+void print₋list(struct tabcontext * tabs, struct dynamic₋bag₋cons * list)
+{  struct dynamic₋bag₋cons * cell = list; struct dynamic₋bag * memory;
+again:
+   if (cell == 0) return;
+   memory=cell->item;
+   switch (memory->T)
    {
-   case callsym: print("⬚call '⬚'\n", ﹟indent(0,tabs), ﹟ident(item->X.store.regular)); break;
-   case branch₋goto₋optsym: print("⬚branch ", ﹟indent(0,tabs), ﹟ident(item->X.store.regular)); break;
-   case label: break;
-   case afterward: break;
-   case comparesym: break;
-   case additionssym: stmt₋print(tabs,item); break;
-   case constsym: print("constant '⬚'\n", ﹟ident(item->X.store.regular)); print₋tree(tabs,item->element); break;
-   case varsym: print("variable '⬚'\n", ﹟ident(item->X.store.regular)); print₋tree(tabs,item->element); break;
-   case procsym: print("transcript '⬚'\n", ﹟ident(item->X.store.regular)); 
-    list₋print(tabs,item->formalºª,item->formal₋last); stmt₋print(tabs,item->sequence); break;
-   default: print("unknown item and symbol-type '⬚'\n",﹟d(item->T));
+   case callsym:
+     print("⬚call '⬚'\n",﹟indent(0,tabs),﹟ident(memory->X.store.regular));
+     break;
+   case branch₋goto₋optsym:
+     print("⬚branch to '⬚'\n",﹟indent(0,tabs),﹟ident(memory->X.store.regular));
+     break;
+   case comparesym:
+     print("⬚compare\n",﹟indent(0,tabs));
+     ++tabs->indentation;
+     print₋expr(tabs,memory->form.element);
+     print₋list(tabs,memory->form.compare₋thenºª);
+     print₋list(tabs,memory->form.compare₋elseºª);
+     --tabs->indentation;
+     break;
+   case afterward:
+     print("⬚assign\n",﹟indent(0,tabs));
+     ++tabs->indentation;
+     print₋expr(tabs,memory->form.l);
+     print₋expr(tabs,memory->form.r);
+     --tabs->indentation;
+     break;
+   case label:
+     print("label '⬚'",﹟ident(memory->X.store.regular));
+     break;
+   case additionssym:
+     print("additions '⬚'",﹟ident(memory->X.store.regular));
+     print₋expr(tabs,memory);
+     break;
+   case constsym:
+     print("constant '⬚'\n",﹟ident(memory->X.store.regular));
+     print₋expr(tabs,memory->form.element);
+     break;
+   case varsym:
+     print("variable '⬚'\n",﹟ident(memory->X.store.regular));
+     print₋expr(tabs,memory->form.element);
+     break;
+   case procsym:
+     print("transcript '⬚'\n",﹟ident(memory->X.store.regular));
+     print₋list(tabs,memory->form.formalºª);
+     print₋list(tabs,memory->form.sequenceºª);
+     break;
+   default:
+     print("unknown list item and symbol-type '⬚'\n",﹟d(memory->T));
    }
+   cell=cell->nxt.next;
+   goto again;
+}
+
+void print₋ast(struct dynamic₋bag * tree)
+{  struct tabcontext tabs; tabs.indentation=0;
+   print₋list(&tabs,tree->form.recollectºª);
+   tabs.indentation=0;
+   print₋list(&tabs,tree->form.machineºª);
 }
 
