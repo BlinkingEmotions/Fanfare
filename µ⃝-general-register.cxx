a83f84d19bb908e09593164b405c4c3493edb6f6
andeha <andeha@users.noreply.github.com>
2023-02-20 01:26:18 +0100

    Improved exposition.

--- µ⃝-general-register.cxx
+++ µ⃝-general-register.cxx
@@ -1,43 +1,64 @@
 /*  µ⃝-general-register.cxx | register allocation. */
 
-void general₋register(struct dynamic₋bag * initial)
-{ struct dynamic₋bag * item=initial;
-   typedef void (^Init)();
-   Init ground = ^{
-     item->l->leg=1,item->r->leg=2,assign(item->l),assign(item->r); /* labels not registers. */
-     item->memory = (item->l->memory==item->r->memory) ? item->l->memory + 1 : 
-      max(2,max(item->l->memory,item->r->memory));
-   };
-   Init leaf = ^{ if (leq==1) { item->memory=0; } else { item->memory=1; } };
+void color₋leaf(struct dynamic₋bag * item)
+{
+   item->memory = (item->leg == 2) ? 1 : 0;
+}
+
+void color₋ground(struct dynamic₋bag * item)
+{
+   item->form.l->leg = 1;
+   item->form.r->leg = 2;
+   color₋ground(item->form.l);
+   color₋ground(item->form.r);
+   int regno = item->form.l->memory + 1;
+   if (item->form.l->memory != item->form.r->memory) {
+     int nxt = max(item->form.l->memory,item->form.r->memory);
+     regno = max(2,nxt);
+   }
+   item->memory = regno;
+}
+
+void color₋sequence(struct dynamic₋bag₋cons * start₋list)
+{ struct dynamic₋bag₋cons * memory = start₋list;
+again:
+   if (memory == 0) goto unagain;
+   memory = memory->nxt.next;
+unagain:
+   return;
+}
+
+void general₋register(struct dynamic₋bag * ast)
+{
+   struct dynamic₋bag * item=ast;
 again:
    if (item==ΨΛΩ) { return; }
    switch (item->T)
    {
-   case ident: leaf(); break;
-   case number: leaf(); break;
-   case times: ground(); break;
-   case divide: ground(); break;
-   case plus: ground(); break;
-   case minus: ground(); break;
-   case eql: ground(); break;
-   case neq: ground(); break;
-   case lss: ground(); break;
-   case leq: ground(); break;
-   case gtr: ground(); break;
-   case geq: ground(); break;
+   case ident: color₋leaf(item); break;
+   case number: color₋leaf(item); break;
+   case times: color₋ground(item); break;
+   case divide: color₋ground(item); break;
+   case plus: color₋ground(item); break;
+   case minus: color₋ground(item); break;
+   case eql: color₋ground(item); break;
+   case neq: color₋ground(item); break;
+   case lss: color₋ground(item); break;
+   case leq: color₋ground(item); break;
+   case gtr: color₋ground(item); break;
+   case geq: color₋ground(item); break;
    case callsym: break;
    case branch₋goto₋optsym: break;
-   case elsesym: assign(item->sequence); break;
-   case thensym: assign(item->sequence); break;
-   case ifsym: assign(item->sequence); break;
-   case afterward: assign(item->r); break;
-   case constsym: assign(item->const₋machineºª); break;
-   case varsym: assign(item->recollectºª); break;
-   case procsym: assign(item->machineºª); break;
-   case oddsym: assign(item->sequence); break;
-   case end₋of₋transmission₋and₋file: break;
+   case elsesym: color₋sequence(item->form.compare₋thenºª); break;
+   case thensym: color₋sequence(item->form.compare₋elseºª); break;
+   case comparesym: color₋sequence(item->form.sequenceºª); break;
+   case afterward: color₋ground(item->form.r); break;
+   case constsym: color₋sequence(item->form.const₋machineºª); break;
+   case varsym: color₋sequence(item->form.recollectºª); break;
+   case procsym: color₋sequence(item->form.machineºª); break;
+   case oddsym: color₋ground(item->form.element); break;
+   case eot₋and₋file: break;
    default: error(3,"unknown symbol-type at assign"); break;
    }
-   item=item->nextºª; goto again;
 }
 
