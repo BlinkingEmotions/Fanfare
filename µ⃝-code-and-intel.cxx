fe5318548ea97d954ad34730c17a9b09bfd83dfd
andeha <andeha@users.noreply.github.com>
2023-02-28 07:28:04 +0100

    Improved exposition.

--- µ⃝-code-and-intel.cxx
+++ µ⃝-code-and-intel.cxx
@@ -221,7 +221,7 @@ void codegenerate()
 "\n    .text\n\n"
    );
    struct dynamic₋bag * material; Nonabsolute symbol;
-   struct dynamic₋bag₋cons *cell₋machine=tree->form.machineºª,*statement₋cell;
+   struct dynamic₋bag₋cons *cell₋machine=td₋tree->form.machineºª,*statement₋cell;
 function₋again:
    if (cell₋machine == 0) goto function₋unagain;
    material = cell₋machine->item;
