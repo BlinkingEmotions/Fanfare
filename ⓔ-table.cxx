6f5c023a5bd9692303d4f6429b856340239c0896
andeha <andeha@users.noreply.github.com>
2023-03-06 17:50:36 +0100

    Improved exposition.

---
+++ ⓔ-table.cxx
@@ -0,0 +1,26 @@
+/*  ⓔ-table.cxx | render table with scalars and intervals. */
+
+int next₋token₋inner₂(Translation * t, struct token₋detail * detail₋out)
+{
+again:
+   i=t₋ctxt->tip₋unicode,t₋ctxt->tip₋unicode+=1;
+   if (i >= symbols) confess(completion);
+   return 0;
+}
+
+int next₋token₋table(Translation * t)
+{
+   return 0;
+}
+
+int Tableparse(struct Unicodes program, char8₋t * path, 
+ Translation * t)
+{
+   return 0;
+}
+
+int Rendertable(Translation * t, Simulator * sim, 
+ chronology₋instant when)
+{
+   return 0;
+}
