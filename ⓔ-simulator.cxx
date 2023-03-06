7e57afe68bf6694f0f8b97bc55c61322f260ff42
andeha <andeha@users.noreply.github.com>
2023-03-06 17:28:04 +0100

    Improved exposition.

--- ⓔ-simulator.cxx
+++ ⓔ-simulator.cxx
@@ -4,14 +4,63 @@
 #define EXIT_APPLICATION 2
 #define CONTINUE_INTERACT 3
 
+int list(char * concept)
+{
+   return CONTINUE_INTERACT;
+}
+
+int general(char * entity)
+{
+   return CONTINUE_INTERACT;
+}
+
+int inspect(char * entity)
+{
+   return CONTINUE_INTERACT;
+}
+
+int step(char * granularity)
+{
+   return CONTINUE_INTERACT;
+}
+
+int table(char * tablepath)
+{
+   return CONTINUE_INTERACT;
+}
+
+int 
+int Execute(int count, char * seqce[])
+{
+   if (strcmp(seqce[0], "quit") == 0) { return EXIT_APPLICATION; }
+   else if (strcmp(seqce[0], "help") == 0) { 
+     print(
+     "this shell responds to a selection of keywords with zero "
+     "alternatively one argument."); }
+   else if (strcmp(seqce[0], "continue") == 0) { return EXIT_INTERACTIVE; }
+   else if (strcmp(seqce[0], "list") && count == 2) { return list(seqce[1]); }
+   else if (strcmp(seqce[0], "general") && count == 2) { return general(seqce[1]); }
+   else if (strcmp(seqce[0], "inspect") && count == 2) { return inspect(seqce[1]); }
+   else if (strcmp(seqce[0], "step") && count == 2) { return step(seqce[1]); }
+   else if (strcmp(seqce[0], "table") && count == 2) { return table(seqce[1]); }
+   else print("unknown turbine command '' (_ parameters).\n");
+   return CONTINUE_INTERACT;
+}
+
 int EvaluateCommand(char8₋t * entered)
 {
    print("Entered '⬚'.\n", ﹟s8(entered));
+   const char * delim = " \t"; char * state;
+   char * first = strtok_r(entered,delim,&state);
+   if (first == NULL) return CONTINUE_INTERACT;
+   char * second = strtok_r(NULL,delim,&state);
+   if (second == NULL) return Execute(1,{first});
+   else return Execute(2,{first,second});
    return CONTINUE_INTERACT;
-} /* recognize keyword and one alternatively two arguments: 
+} /* recognize keyword and zero alternatively one arguments: 
   quit, help, list schedule|entity|currency|exchange, 
-  general <entity>, inspect <entity>, 
-  continue, step statement|iteration. */
+  general <entity>, inspect <entity>, continue, step 
+  statement|iteration, table <filepath>. */
 
 void EnterInteractiveMode(int * quit, Simulator * 🅢)
 { int y=0; 🅢->prompt_number=1;
