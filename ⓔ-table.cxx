6e4018c8051f26003a09c4001c12f25c7b541650
andeha <andeha@users.noreply.github.com>
2023-03-07 11:27:41 +0100

    Improved exposition.

--- ⓔ-table.cxx
+++ ⓔ-table.cxx
@@ -1,32 +1,87 @@
 /*  ⓔ-table.cxx | render table with scalars and intervals. */
 
-int next₋token₋inner₂(Translation * t, struct token₋detail * detail₋out)
+int next₋token₋inner₂(Translation * t, Symbol * detail₋out)
 { __builtin_int_t i,symbols=t->ctxt.program₋text.tetras; char32̄_t uc,uc₊₁,uc₊₂;
    int lift₋count=0,sym;
-   🧵(identifer,keyword,trouble,completion)
+   🧵(identifer,keyword,unicodes,trouble,completion)
    {
-   case completion: break;
-   case trouble: break;
+   case identifer: copy₋identifier(&t->ctxt,t->ident,detail₋out); t->ctxt.syms₋in₋regular=0; 
+    t->ctxt.state=mode₋initial; return 0;
+   case keyword: assign₋symbol₋noforward(sym,detail₋out,&t->ctxt,t->ctxt.syms₋in₋regular); 
+    t->ctxt.syms₋in₋regular=0; t->ctxt.state=mode₋initial; return 0;
+   case unicodes: return 0;
+   case completion: assign₋symbol(eot₋and₋file,detail₋out,&t->ctxt,0); return 0;
+   case trouble: vfprint("trouble occurred at ⬚.\n",﹟d(t->ctxt.tip₋unicode));
+    return -1;
    }
 again:
    i=t->ctxt.tip₋unicode,t->ctxt.tip₋unicode+=1;
    if (i >= symbols) confess(completion);
-   return 0;
+   if (i == symbols - 1) lift₋count=2;
+   if (i == symbols - 2) lift₋count=1;
+   uc = *(t->ctxt.program₋text.unicodes + i);
+   uc₊₁ = lift₋count >= 2 ? U' ' : *(t->ctxt.program₋text.unicodes + i + 1);
+   uc₊₂ = lift₋count >= 1 ? U' ' : *(t->ctxt.program₋text.unicodes + i + 2);
+   if (STATE(mode₋initial) && uc == U'\xa') {
+     location₋nextline(&t->ctxt.interval);
+   }
+   ELIF₋INIT₋WITH₋ONE(U'\xd') { /* do nothing */ }
+   ELIF₋INIT₋WITH₋ONE(U' ') { location₋legion(&t->ctxt.interval); }
+   ELIF₋INIT₋WITH₋ONE(U'\t') { location₋legion(&t->ctxt.interval); }
+   ELIF₋INIT₋WITH₋ONE(U'*') { assign₋symbol(times,detail₋out,1); RET }
+   ELIF₋INIT₋WITH₋ONE(U'/') { assign₋symbol(divide,detail₋out,1); RET }
+   ELIF₋INIT₋WITH₋ONE(U'+') { assign₋symbol(plus,detail₋out,1); RET }
+   ELIF₋INIT₋WITH₋ONE(U'-') { assign₋symbol(minus,detail₋out,1); RET }
+   else if (STATE(mode₋single₋ekunem) && uc == '\xa') t->ctxt.state=mode₋initial;
+   else if (STATE(mode₋multi₋ekunem) && uc == U'*' && uc₊₁ == U'/') { t->ctxt.tip₋unicode+=1,t->ctxt.state=mode₋initial; }
+   else if (STATE(mode₋initial) && uc == U'/' && uc₊₁ == U'*') t->ctxt.state=mode₋multi₋ekunem;
+   else if (STATE(mode₋initial) && uc == U'/' && uc₊₁ == U'/') t->ctxt.state=mode₋single₋ekunem;
+   else if (STATE(mode₋initial) && uc == U'\'') {
+     t->ctxt.reference₋quoted = collection₋count(text₋unicode);
+     t->ctxt.syms₋in₋quotes=0;
+     t->ctxt.state = mode₋quotes₋text;
+     location₋legion(&ctxt->interval);
+   }
+   else if (STATE(mode₋quotes₋text)) {
+     if (uc == U'\'') {
+       if (regularpool₋datum₋text(text₋unicode...))
+       else { if}
+     }
+     confess()
+   }
+   else if ((STATE(mode₋initial) && letter(uc)) || (STATE(mode₋regular) && (letter(uc) || digit(uc)))) {
+     
+   }
+   EL₋CONFESS
+   goto again;
 }
 
 int next₋token₋table(Translation * t)
+{ int y;
+   if (t->ctxt.tip₋unicode == 0) {
+     y = next₋token₋inner₂(t,&t->symbol);
+     if (y != 0) { Diagnos(1,t->ctxt.source₋path,&t->symbol.gritty.interval,1, 
+      "table scanner error, initial trouble"); exit(2); }
+   } else {
+     t->symbol₋passed = t->symbol;
+     t->symbol = t->retrospect;
+   }
+   y = next₋token₋inner₂(t,&t->retrospect);
+   if (y != 0) { Diagnos(1,t->ctxt.source₋path,&t->retrospect.gritty.interval, 
+    1,"table scanner error, advance failure"); exit(3); }
+#if defined TRACE₋TOKENS
+   void trace₋token(Symbol, struct collection *);
+   trace₋token(t->symbol,t->ident);
+#endif
+   return 0;
+}
+
+int Tableparse(struct Unicodes program, char8₋t * path, Translation * t)
 {
    return 0;
 }
 
-int Tableparse(struct Unicodes program, char8₋t * path, 
- Translation * t)
-{
-   return 0;
-}
-
-int Rendertable(Translation * t, Simulator * sim, 
- chronology₋instant when)
+int Rendertable(Translation * t, Simulator * sim, chronology₋instant when)
 {
    return 0;
 }
