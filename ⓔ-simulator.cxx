c48f40e2a8f1a9a5dbdc6fd9cca00a5f3b328268
andeha <andeha@users.noreply.github.com>
2023-03-09 06:16:01 +0100

    Improved exposition.

--- ⓔ-simulator.cxx
+++ ⓔ-simulator.cxx
@@ -1,4 +1,30 @@
-/*  ⓔ-simulator.cxx |  movement Brownian and advance like Monte-Carlo. */
+/*  ⓔ-simulator.cxx | movement Brownian and advance like Monte-Carlo. */
+
+int Conversion(Instant instant, Real value, Nonabsolute src, Nonabsolute dst, Real * also)
+{
+   return 0;
+}
+
+int IsReversed(Nonabsolute left, Nonabsolute right, struct collection * ident)
+{
+   __builtin_int_t left₋ment = regularpool₋segments(left,ident);
+   __builtin_int_t right₋ment = regularpool₋segments(right,ident);
+   short symbols1[left₋ment]; char32̄_t * segment1[left₋ment];
+   short symbols2[right₋ment]; char32̄_t * segment2[right₋ment];
+   if (regularpool₋token(left₋ment,symbols1,segment1,ident)) return -1;
+   if (regularpool₋token(right₋ment,symbols2,segment2,ident)) return -1;
+   int cl=0,cr=0,sl=0,sr=0; char32̄_t ucl,ucr; int ldx=1,rdx=1;
+again:
+   if (sl >= left₋ment || sr >= right₋ment) goto unagain;
+   ucl = *(char32̄_t *)collection₋relative(ldx,ident);
+   ucr = *(char32̄_t *)collection₋relative(rdx,ident);
+   if (ucr != ucl) goto notagain;
+   ldx+=1,cl+=1; if (cl == segment1[sl]) { cl=0,sl+=1; }
+   rdx+=1,cr+=1; if (cr == segment2[sr]) { cr=0,sr+=1; }
+   goto again;
+notagain:
+   return sl == left₋ment && sr == right₋ment;
+}
 
 #define EXIT_INTERACTIVE 1
 #define EXIT_APPLICATION 2
@@ -6,6 +32,10 @@
 
 int list(char * concept)
 {
+   if (strcmp(concept,"schedule") == 0) { }
+   else if (strcmp(concept,"entity") == 0) { }
+   else if (strcmp(concept,"currency") == 0) { }
+   else if (strcmp(concept,"exchange") == 0) { }
    return CONTINUE_INTERACT;
 }
 
@@ -35,9 +65,10 @@ int Execute(int count, char * seqce[])
    else if (strcmp(seqce[0], "help") == 0) { 
      print(
      "The turbin shell responds to a selection of keywords with zero "
-     "alternatively one argument:\n\n  list schedule|entity|"
-     "currency|exchange,\n  general <entity>,\n  inspect <entity>,\n  continue,\n  "
-     "step statement|iteration,\n  table <filepath>,\n  'quit',\n  'help'.\n\n"); }
+     "alternatively one argument:\n\n · list schedule|entity|"
+     "currency|exchange,\n · general <entity>,\n · inspect <entity>,\n · "
+     "continue,\n · step statement|iteration,\n · table <filepath>,\n · "
+     "quit,\n · help.\n\n"); }
    else if (strcmp(seqce[0], "continue") == 0) { return EXIT_INTERACTIVE; }
    else if (strcmp(seqce[0], "list") && count == 2) { return list(seqce[1]); }
    else if (strcmp(seqce[0], "general") && count == 2) { return general(seqce[1]); }
