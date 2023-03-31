2128110b3e97bdbc079b9920aec144f349794464
andeha <andeha@users.noreply.github.com>
2023-03-31 06:46:05 +0200

    Improved exposition.

--- ╳-runlink-symbols.cxx
+++ ╳-runlink-symbols.cxx
@@ -1,15 +1,16 @@
 /*  ╳-runlink-symbols.cxx - internal references used inside machine translation. */
 
 enum symbol₋class { diffusesym, preproc₋if, preproc₋else, preproc₋elif, 
- preproc₋end, preproc₋include, preproc₋system, preproc₋defined, preproc₋define, 
- partialsym, fostratdefisym, structsym, definitesym, end₋and₋dotsym, 
- big₋endiansym, little₋endiansym, unionsym, apriorisym, typedefsym, 
- constantsym, computesym, collide, comparesym, ifsym, gotosym, transcriptsym, 
- inexorablesym, mentativesym, startsym, inlinesym, coroutinesym, endsym, 
- additionssym, ident, number, assym /* lāngs utstreckning */, indirectsym, 
- voluntarysym, isomorphsym, iteratesym, iteratedsym, referssym, intsym, 
- char8₋tsym, char32̄_tsym, binary32sym, decimal32sym, tertary32sym, 
+ preproc₋end, preproc₋include, preproc₋system, preproc₋defined, 
+ preproc₋define, partialsym, fostratdefisym, structsym, definitesym, 
+ end₋and₋dotsym, big₋endiansym, little₋endiansym, unionsym, apriorisym, 
+ typedefsym, constantsym, computesym, collide, comparesym, ifsym, /* likely and unlikely */
+ gotosym, transcriptsym, inexorablesym, mentativesym, startsym, inlinesym, 
+ coroutinesym, endsym, additionssym, ident, number, assym /* lāngs utstreckning */, 
+ indirectsym, voluntarysym, isomorphsym, iteratesym, iteratedsym, referssym, 
+ intsym, char8₋tsym, char32̄_tsym, binary32sym, decimal32sym, tertary32sym, 
  binary128sym, decimal128sym, tertary128sym, unsignedsym, schemasym, 
  attributessym, prominentsym, addressofsym, nonaltersym, 
  end₋of₋transmission₋and₋file };
+
 
