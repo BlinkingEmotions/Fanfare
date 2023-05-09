56c1b1de51fe6ff2caabef21d1efefe8281c9be7
andeha <andeha@users.noreply.github.com>
2023-05-09 22:12:33 +0200

    Improved exposition.

--- ∎|∎ indirect.c
+++ ∎|∎ indirect.c
@@ -7,19 +7,19 @@ typedef struct { __builtin_uint_t address,bytes; } refers;
 void load(int * value, refers ref) ⓣ { ref.address = (__builtin_uint_t)value; ref.bytes=sizeof(int); }
 void dereference(refers ref, int * value) ⓣ { *value = *(int *)(ref.address); }
 
-int foo(refers input)
+void foo(refers input, void (^completion)(refers output))
 {
-   int value;
-   dereference(input,&value);
-   return value;
+   completion(input);
 }
 
 int main(int argc, char * argv[])
 {
    int value1=17; refers indirect1;
    load(&value1,indirect1);
-   int value2 = foo(indirect1); 
+   foo(indirect1, ^(refers output) { int value2;
+     dereference(output,&value2);
+     print("value2 is ⬚\n", ﹟d((__builtin_int_t)value2));
+   }); 
 }
 
 /*   clang -g -I../Apps/ '∎|∎ indirect.c' ../Apps/Source/Releases/libTwinbeam-x86_64.a */
-
